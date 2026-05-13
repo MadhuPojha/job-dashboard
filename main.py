2b8e109530d60092a4c51963b17623f3bf6f8681
Madhu Priyadarshni Ojha <165268887+MadhuPojha@users.noreply.github.com>
2026-05-13 10:01:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from jobs import get_jobs, refresh_jobs
+from fastapi.responses import FileResponse
+from jobs import get_jobs
 
 app = FastAPI()
 
 
-# API (FAST - uses CACHE)
-@app.get("/api/jobs")
-def jobs_api():
-    return get_jobs()
-
-
-# MANUAL REFRESH (optional)
-@app.get("/api/refresh")
-def refresh():
-    return {"status": "refreshing", "jobs": refresh_jobs()}
-
-
-# FRONTEND
 @app.get("/")
 def home():
-    with open("index.html", "r", encoding="utf-8") as f:
-        return HTMLResponse(f.read())
+    return FileResponse("index.html")
+
+
+@app.get("/api/jobs")
+def jobs():
+    return get_jobs()
