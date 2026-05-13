c2c06a52797141fbef28214f0eb9d09c9da5ecba
Madhu Priyadarshni Ojha <165268887+MadhuPojha@users.noreply.github.com>
2026-05-13 10:02:05 +0200

    Refactor cache structure to use list and timestamp

--- cache.py
+++ cache.py
@@ -1,19 +1,2 @@
-import time
-
-CACHE = {
-    "data": [],
-    "timestamp": 0
-}
-
-CACHE_TTL = 300  # 5 min
-
-
-def get_cache():
-    if time.time() - CACHE["timestamp"] < CACHE_TTL:
-        return CACHE["data"]
-    return None
-
-
-def set_cache(data):
-    CACHE["data"] = data
-    CACHE["timestamp"] = time.time()
+CACHE = []
+LAST_UPDATED = 0
